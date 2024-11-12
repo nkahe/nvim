37b03ce10968d16f6be756fd1fdcc4655aced8c7
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-12 07:54:18 +0200

    fix: autosource

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,17 +2,49 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Get Neovim's config home directory
-local config_dir = vim.fn.stdpath("config") .. "/lua/config/"
 
--- Set up an autocommand for sourcing files upon saving in the "lua/config/" directory
+-- -- Simple test autocommand to confirm Neovim can respond to saves
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "*.lua",  -- Match any Lua file
+--   callback = function()
+--     print("File saved!")
+--   end,
+-- })
+
+-- Get Neovim's config home directory
+-- local config_dir = vim.fn.stdpath("config") .. "/lua/config/"
+
+-- -- Set up an autocommand for sourcing files upon saving in the "lua/config/" directory
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = config_dir .. "*.lua",
+--   callback = function(args)
+--     vim.cmd("source " .. args.file)
+--     print("Sourced " .. args.file)
+--   end,
+--   desc = "Automatically source files on save in lua/config/",
+-- })
+
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "lua/config/*.lua",
+--   callback = function(args)
+--     vim.cmd("source " .. args.file)
+--     print("Sourced " .. args.file)
+--   end,
+--   desc = "Automatically source files on save in lua/config/",
+-- })
+
+
+-- Watches for saves specifically in the `lua/config/` directory
+local config_dir_pattern = vim.fn.stdpath("config") .. "/lua/config/*.lua"
+
+print(config_dir_pattern)
+
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = config_dir .. "*.lua",
+  pattern = config_dir_pattern,
   callback = function(args)
     vim.cmd("source " .. args.file)
-    print("Sourced " .. args.file)
+    print("Reloaded " .. args.file)
   end,
-  desc = "Automatically source files on save in lua/config/",
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
