cad0d0712654198c6565b34f5bc21c2e1f58b46f
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-13 11:47:04 +0200

    diagnostics virtualtext pois päältä

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,11 +19,58 @@ vim.g.autoformat = false
 
 vim.opt.colorcolumn = "80"
 
+-- vim.diagnostic.config({
+--   virtual_text = false,
+--   float = {
+--     focusable = false,  -- Makes the floating window non-interactive
+--     border = "rounded",  -- Add a border around the floating window
+--     source = true,   -- Show the source of the diagnostic (e.g., LSP server)
+--     -- severity_sort = true,  -- Sort diagnostics by severity
+--   },
+-- })
+
 vim.opt.scrolloff = 7
 
 vim.opt.updatetime = 5000
 
+-- Cursor can go paste last character and after block
 vim.opt.virtualedit:append("block,onemore")
+
+
+-- vim.diagnostic.config({
+--   virtual_text = {
+--     prefix = "●",  -- Change the symbol for better visibility
+--     spacing = 10,  -- Increase spacing (may help move it a bit further right, but not a large change)
+--   },
+--   float = {
+--     focusable = false,
+--     border = "rounded",
+--     source = true,
+--     severity_sort = true,
+--   },
+-- })
+
+-- -- Set the diagnostic configuration for Neovim
+-- vim.diagnostic.config({
+--   virtual_text = false,  -- Disable inline virtual text
+--   signs = true,          -- You can still see the signs in the gutter (e.g., warning/error icons)
+--   underline = true,      -- Optionally underline the lines with diagnostics
+--   update_in_insert = true,  -- Update diagnostics while typing
+--   float = {
+--     focusable = false,  -- Makes the floating window non-interactive
+--     border = "rounded",  -- Add a border around the floating window
+--     source = true,   -- Show the source of the diagnostic (e.g., LSP server)
+--     severity_sort = true,  -- Sort diagnostics by severity
+--     max_width = 80,         -- Optional: Restrict max width of the floating window
+--     relative = "cursor",   -- Position relative to the cursor
+--     col = 80,              -- Start the floating diagnostic at column 80
+--   },
+-- })
+
+-- Optionally you can use an autocommand to automatically open floating diagnostics for LSP errors
+-- vim.cmd([[
+--   autocmd CursorHold * lua vim.diagnostic.open_float({scope="line"})
+-- ]])
 
 -- GUI ------------------------------------------
 
