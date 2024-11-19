b943f3d36e34c5920e3cb2ffe00e989305527c10
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-19 12:18:05 +0200

    optionit nätimmin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,25 +6,33 @@
 
 -- No format on save.
 vim.g.autoformat = false
-vim.opt.colorcolumn = "80"
-vim.opt.selectmode = "key"
-vim.opt.scrolloff = 7
-vim.opt.spell = false
-vim.opt.title = true
-vim.opt.updatetime = 5000
+
+local options = {
+  colorcolumn = "80",
+  guifont = { "FiraCode Nerd Font", ":h12" },
+  selectmode = "key",
+  scrolloff = 7,
+  spell = false,
+  title = true,
+}
+
+for option, value in pairs(options) do
+  vim.opt[option] = value
+end
+
 -- Cursor can go paste last character and after block
 vim.opt.virtualedit:append("block,onemore")
-
 -- Optionally you can use an autocommand to automatically open floating diagnostics for LSP errors
 -- vim.cmd([[
 --   autocmd CursorHold * lua vim.diagnostic.open_float({scope="line"})
 -- ]])
 
+vim.api.nvim_set_hl(0, 'NeoTreeGitUntracked', { fg = '#73c991' })
+
 -- GUI ------------------------------------------
 
 -- Neovide
 if vim.g.neovide then
-  vim.opt.guifont = { "FiraCode Nerd Font", ":h12" }
   vim.g.neovide_cursor_animation_length = 0.03
   vim.g.neovide_cursor_trail_size = 0.08
 end
