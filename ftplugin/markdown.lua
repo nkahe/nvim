04fb42c8395dd123ced5ef22355d56cd70a78396
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-19 12:11:53 +0200

    markdown.lua root -> after

--- ftplugin/markdown.lua
+++
@@ -1,8 +0,0 @@
-
--- Doesn't seem to work with LazyVim.
-vim.opt_local.spell = false
-
--- Enable folding
-vim.opt.foldenable = true         -- Enable folding
-vim.opt.foldlevel = 99            -- Open most folds by default
-vim.opt.foldmethod = "expr"     -- Default fold method (you can change this)
