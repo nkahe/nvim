61a43dd07672492519260dfb4d1384fa3f9bec40
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-22 16:05:41 +0200

    add neotree winbar

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    sources = { "filesystem", "buffers", "git_status" },
+    source_selector = {
+      winbar = true,
+      content_layout = "center",
+    },
+
+  }
+
+}
