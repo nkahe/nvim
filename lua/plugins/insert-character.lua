81bb95ff8b2a257623cee71ba16982f735a4831f
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-24 16:22:50 +0200

    insert character toimimaan vscodessa

--- lua/plugins/insert-character.lua
+++ lua/plugins/insert-character.lua
@@ -1,11 +1,11 @@
 -- bagohart/Vim-Insert-Single-Character
 -- https://github.com/bagohart/vim-insert-append-single-character
 
--- TODO: binding tällekin: <Plug>(ISC-append-at-cursor)
 return {
   "bagohart/vim-insert-append-single-character",
   keys = {
     { "ä", "<Plug>(ISC-insert-at-cursor)", mode = "n", desc = "Insert character before cursor" },
-    { "Ä", "<Plug>(ISC-append-at-cursor)", mode = "n", desc = "Insert character before cursor" },
+    { "Ä", "<Plug>(ISC-append-at-cursor)", mode = "n", desc = "Insert character after cursor" },
   },
+  vscode = true
 }
