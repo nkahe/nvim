d92f9645838377b8e0edff41af8432b1241d2465
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-25 12:56:35 +0200

    Add restore session vaihtoehdot

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,5 +7,21 @@ return {
       enabled = true,
       timeout = 6000,
     },
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore last session", action = function() require("persistence").load({ last = true }) end },
+          { icon = " ", key = "S", desc = "Restore session", action = function() require("persistence").select() end },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
   },
 }
