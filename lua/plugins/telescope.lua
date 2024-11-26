e1ab8fc1e816176d2d0d1dd4b9c4c3987f9c54f9
Henri K <12832691+nkahe@users.noreply.github.com>
2024-11-26 18:31:07 +0200

    telecope exclude

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
  "nvim-telescope/telescope.nvim",
  keys = { {"<C-P>", "<cmd>Telescope find_files<cr>", desc = "Find Files"} },
+  opts = function(_, opts)
+    -- Add ignore patterns to existing configuration
+    opts.defaults = opts.defaults or {}
+    opts.defaults.file_ignore_patterns = vim.list_extend(
+      opts.defaults.file_ignore_patterns or {},
+      { "node_modules/", "%.sqlite$", "%.png$", "%.jpg$", "%.jpeg$", "%.gif$", "%.bmp$", "%.tiff$", "%.webp$" }
+    )
+  end,
 }
