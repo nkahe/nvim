c9609be6ab04deaa0ecf4abd034d79b636aa0f8e
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-10 16:03:14 +0200

    vscode mappingeja

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -3,6 +3,24 @@
 
 -- vim.cmd("echo 'sourced vscode.lua'")
 
-vim.keymap.set({ 'n', 'x' }, '<Space>', function()
-    vim.fn.VSCodeNotify('whichkey.show')
-  end, { silent = true, desc = "Show WhichKey" })
+-- vim.keymap.set({ 'n', 'x' }, '<Space>', function()
+--     vim.fn.VSCodeNotify('whichkey.show')
+--   end, { silent = true, desc = "Show WhichKey" })
+--
+
+vim.keymap.set('n', '<leader><leader>', function()
+    vim.fn.VSCodeNotify('workbench.action.quickOpen')
+  end, { desc = 'QuickOpen' })
+
+vim.keymap.set({ 'n', 'x' }, '<Space>e', function()
+    vim.fn.VSCodeNotify('workbench.action.toggleSidebarVisibility')
+  end, { silent = true, desc = "Toggle Sidebar Visibility" })
+
+
+vim.keymap.set({ 'n', 'x' }, '<Space>fr', function()
+    vim.fn.VSCodeNotify('workbench.action.openRecent')
+  end, { silent = true, desc = "Recent files" })
+
+vim.keymap.set({ 'n', 'x' }, '<Space>fn', function()
+    vim.fn.VSCodeNotify('welcome.showNewFileEntries')
+  end, { silent = true, desc = "New file" })
