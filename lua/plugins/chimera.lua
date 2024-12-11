c62b0ae0ab05c6170660b378136d0a8f1630fa96
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-11 15:40:34 +0200

    clonecom plugin nimi -> chimera

---
+++ lua/plugins/chimera.lua
@@ -0,0 +1,12 @@
+-- Tris / nvim-chimera · GitLab - https://gitlab.com/Biggybi/nvim-chimera
+-- An easy way to create a commented version of a set of lines.
+
+return {
+  "https://gitlab.com/Biggybi/nvim-chimera.git",
+  dependencies = {
+    -- pick one or none
+    -- "tpope/vim-commentary"
+    "numToStr/Comment.nvim",
+  },
+  opts = {},
+}
--- lua/plugins/clonecom.lua
+++
@@ -1,9 +0,0 @@
--- clonecom - clone and comment lines - GitLab https://gitlab.com/Biggybi/nvim-clonecom
--- An easy way to create a commented version of a set of lines.
-
-  return {
-    "https://gitlab.com/Biggybi/nvim-dupascomment.git",
-    -- optional
-    -- dependencies = { "tpope/vim-commentary" },
-    opts = {},
-  }
