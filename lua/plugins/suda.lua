4420c23010c4b59c91eb6cee97f27410f706e239
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-24 04:48:57 +0200

    feat: add suda-plugin

---
+++ lua/plugins/suda.lua
@@ -0,0 +1,6 @@
+-- lambdalisue/vim-suda: 🥪 An alternative sudo.vim for Vim and Neovim,
+-- limited support sudo in Windows - https://github.com/lambdalisue/vim-suda
+
+return {
+  'lambdalisue/vim-suda'
+}
