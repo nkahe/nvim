efcc4963c95ed8a0181fdbb2e77c93ddb72fdf05
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-25 15:47:33 +0200

    fix: [[ ja ]] descs. refactor: user commandit omaksi tiedostoksi

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,6 +24,8 @@ else
   -- end, {})
 end
 
+require("config.user-commands")
+
 -- NOTE: For mappings "{ noremap = true }" is the default so no need to add that.
 
 
@@ -59,8 +61,8 @@ end
 
 -- map({""}, "c", '"_c', { desc = "which_key_ignore" })
 -- map({""}, "C", '"_C', { desc = "which_key_ignore" })
-
 -- Deleting without yanking
+
 map({'n', 'v'}, "<Leader>d", '"_d', { desc = "Delete without yanking" })
 map({'n', 'v'}, "<Leader>D", '"_D', { desc = "Delete to end of line without yanking" })
 map({'n', 'v'}, "<LocalLeader>d", '"_d', { desc = "Delete without yanking" })
@@ -156,72 +158,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<C-]>", { silent = true })
   end,
 })
-
--- Command mode (:) -----------------------------------------
-
-vim.keymap.set("ca", "W", "w")
-vim.keymap.set("ca", "Wq", "wq")
-vim.keymap.set("ca", "WQ", "wq")
-
-local create_cmd = vim.api.nvim_create_user_command
-
-create_cmd('Cdb', function()
-  vim.cmd('cd ' .. vim.fn.expand('%:p:h'))
-  vim.cmd('pwd')
-end, { desc = "Change cwd to match current buffer's directory" })
-
--- Obsidian plugin
-create_cmd('Obs', function()
-  vim.cmd('cd ~/Documents/notes')
-  vim.cmd("vnew")
-  -- vim.cmd("vertical resize 30")
-  -- Go back to the original window
-  vim.cmd("wincmd h")
-  vim.cmd('Neotree show')
-  vim.cmd('sleep 100ms')
-  -- vim.cmd("vertical resize 80")
-  vim.cmd("vertical resize 90")
-  -- Resize the new split to 80 columns
-  -- vim.cmd("edit '~/Documents/notes/1 Projektit/1 Todo.md'")
-end, { desc = "Change to Obsidian notes directory" })
-
--- Trim trailing whitespace from the buffer
-create_cmd('Trim', function()
-  local save_cursor = vim.fn.getpos(".")
-  -- Save view state for visual mode users
-  local save_view = vim.fn.winsaveview()
-  -- Perform the substitution to trim trailing whitespace
-  vim.cmd([[keeppatterns %s/\v\s+$//e]])
-  -- Restore cursor position and view state
-  vim.fn.setpos(".", save_cursor)
-  vim.fn.winrestview(save_view)
-end, { desc = "Trim trailing whitespace from the buffer" })
-
--- Source keymaps.lua and options.lua, and show a notification
-create_cmd("Reload", function()
-  local success_keymaps, success_options, success_autocmds  = false, false, false
-  local basepath = vim.fn.stdpath("config") .. "/lua/config"
-
-  local keymaps_path = basepath .. "/keymaps.lua"
-  success_keymaps = pcall(dofile, keymaps_path)
-
-  local options_path = basepath .. "/options.lua"
-  success_options = pcall(dofile, options_path)
-
-  local autocmds_path = basepath .. "/autocmds.lua"
-  success_autocmds = pcall(dofile, autocmds_path)
-
-  -- Check results and display a message
-  if success_keymaps and success_options and success_autocmds then
-    vim.notify("Sourced keymaps.lua, options.lua, autocmds.lua", vim.log.levels.INFO)
-  elseif not success_keymaps then
-    vim.notify("Failed to source keymaps.lua", vim.log.levels.ERROR)
-  elseif not success_options then
-    vim.notify("Failed to source options.lua", vim.log.levels.ERROR)
-  elseif not success_autocmds then
-    vim.notify("Failed to source autocmds.lua", vim.log.levels.ERROR)
-  end
-end, { desc = "Reload settings" })
 
 -- Restore default mappings ------------------------------
 
@@ -333,12 +269,8 @@ end
 local wk = require("which-key")
 
 wk.add({
-  ["["] = {
-    ["["] = "Previous reference",
-  },
-  ["]"] = {
-    ["]"] = "Next reference",
-  },
+  { "[[", desc = "Previous reference" },
+  { "]]", desc = "Next reference" },
 }, { mode = "n" })
 
 -- Minor tweaks ------------------------------------------
