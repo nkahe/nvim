e0409900dd6c86a3ceeec9b490a3d950419b58c9
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-27 16:32:10 +0200

    cursor line only in active window

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -36,19 +36,34 @@
 --   end,
 -- })
 
+-- show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    if vim.w.auto_cursorline then
+      vim.wo.cursorline = true
+      vim.w.auto_cursorline = nil
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    if vim.wo.cursorline then
+      vim.w.auto_cursorline = true
+      vim.wo.cursorline = false
+    end
+  end,
+})
 
 -- Disable relative numbers in Insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
     pattern = "*",
     command = "set norelativenumber",
 })
-
 -- Enable relative numbers when leaving Insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     command = "set relativenumber",
 })
-
 
 -- Disable diagnostics for these type by default
 vim.api.nvim_create_autocmd("FileType", {
