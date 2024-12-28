8d5f96fdfc87ebf7799282d8a395cfa6eb7584a5
Henri K <12832691+nkahe@users.noreply.github.com>
2024-12-28 14:29:53 +0200

    neoscroll -> archive

---
+++ archive/neoscroll.lua
@@ -0,0 +1,43 @@
+
+-- karb94/neoscroll.nvim: Smooth scrolling neovim plugin written in lua.
+-- https://github.com/karb94/neoscroll.nvim
+
+-- snacks.scroll replaces this.
+if true then return {} end
+
+return {
+  "karb94/neoscroll.nvim",
+  cond = function()
+    -- Only load if not running in a GUI
+    return vim.fn.has("gui_running") == 0
+  end,
+  config = function()
+    local neoscroll = require('neoscroll')
+
+    -- Setup with default easing function
+    neoscroll.setup({
+      easing = "quadratic"
+    })
+
+    -- Define key mappings with specific easing functions
+    local keymap = {
+      -- Use the "sine" easing function
+      ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 200, easing = 'sine' }) end,
+      ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 200, easing = 'sine' }) end,
+      -- Use the "circular" easing function
+      ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 400, easing = 'circular' }) end,
+      ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 400, easing = 'circular' }) end,
+      -- Custom scroll behavior
+      ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
+      ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
+      ["<C-Down>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
+      ["<C-Up>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
+    }
+
+    -- Set key mappings for specified modes
+    local modes = { 'n', 'v', 'x' }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func, { silent = true })
+    end
+  end,
+}
