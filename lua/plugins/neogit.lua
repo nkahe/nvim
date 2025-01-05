81d4e7dfee7d66ac2a63770aebdb5d26eabf2758
Henri K <12832691+nkahe@users.noreply.github.com>
2025-01-05 16:25:02 +0200

    feat: add Neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,31 @@
+
+
+vim.keymap.set("n", "<Leader>gn", "<cmd>Neogit<cr>", { desc = "Neogit" })
+
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed.
+    -- "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",                 -- optional
+    -- "echasnovski/mini.pick",         -- optional
+  },
+  config = true,
+  opts = {
+    commit_editor = {
+      kind = "tab",
+      show_staged_diff = true,
+      -- Accepted values:
+      -- "split" to show the staged diff below the commit editor
+      -- "vsplit" to show it to the right
+      -- "split_above" Like :top split
+      -- "vsplit_left" like :vsplit, but open to the left
+      -- "auto" "vsplit" if window would have 80 cols, otherwise "split"
+      staged_diff_split_kind = "split",
+      spell_check = false,
+    },
+  }
+}
