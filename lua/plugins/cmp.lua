415baf9b7ede0842ae1e7111f4ae0b9e8f642e28
Henri K <12832691+nkahe@users.noreply.github.com>
2025-01-06 04:16:23 +0200

    remove: nvim-cmp

--- lua/plugins/cmp.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    -- Disable ghost text by setting `cmp.config.window` to remove the preview
-    opts.experimental = { ghost_text = false }
-  end,
-}
