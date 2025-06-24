03e1f164f503376717cc1d183356d6cf55e37b97
Henri K <12832691+nkahe@users.noreply.github.com>
2025-06-24 15:02:23 +0300

    update, add workarounds for mason and blink

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,6 @@
+-- LazyVim isn't updatedyet for new Mason version so use old one.
+
+return {
+  { "mason-org/mason.nvim", version = "~1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "~1.0.0" },
+}
